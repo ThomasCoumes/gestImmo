725df5d3f7a345a41acf87eaf202bbefa00542a7
Thomas Coumes <thomas.coumes@smile.fr>
2019-02-11 16:13:24 +0100

    property crud

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Property;
+use App\Form\PropertyType;
+use App\Repository\PropertyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/property")
+ */
+class PropertyController extends AbstractController
+{
+    /**
+     * @Route("/", name="property_index", methods={"GET"})
+     */
+    public function index(PropertyRepository $propertyRepository): Response
+    {
+        return $this->render('property/index.html.twig', [
+            'properties' => $propertyRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="property_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($property);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('property_index');
+        }
+
+        return $this->render('property/new.html.twig', [
+            'property' => $property,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="property_show", methods={"GET"})
+     */
+    public function show(Property $property): Response
+    {
+        return $this->render('property/show.html.twig', [
+            'property' => $property,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="property_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Property $property): Response
+    {
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('property_index', [
+                'id' => $property->getId(),
+            ]);
+        }
+
+        return $this->render('property/edit.html.twig', [
+            'property' => $property,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="property_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Property $property): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$property->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($property);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('property_index');
+    }
+}
