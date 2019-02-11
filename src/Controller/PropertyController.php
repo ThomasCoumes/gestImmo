3c5aa5968d45ae085467c75f7b17e5a9d3db8c5e
Thomas Coumes <thomas.coumes@smile.fr>
2019-02-11 16:15:06 +0100

    isGranted on PropertyController

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -9,9 +9,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/property")
+ * @IsGranted("ROLE_USER")
  */
 class PropertyController extends AbstractController
 {
