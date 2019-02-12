c98e484de54c005c234eb728970b3c1398839393
Thomas Coumes <thomas.coumes@smile.fr>
2019-02-12 16:33:00 +0100

    checkbox for equipment + style

--- src/Entity/Equipment.php
+++ src/Entity/Equipment.php
@@ -14,172 +14,50 @@ class Equipment
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Assert\NotBlank
      * @Assert\Type("integer")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="boolean", nullable=true)
+     * @ORM\Column(type="string", nullable=true)
      */
-    private $elevator;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $cellar;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $garden;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $parking;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $balcony;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $opticalFiber;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $intercom;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $terrace;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $swimmingPool;
+    private $name;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Property", inversedBy="equipments")
      */
     private $equipment;
 
+    /**
+     * @return mixed
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getElevator(): ?bool
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): int
     {
-        return $this->elevator;
+        $this->id = $id;
     }
 
-    public function setElevator(?bool $elevator): self
+    /**
+     * @return mixed
+     */
+    public function getName(): ?string
     {
-        $this->elevator = $elevator;
-
-        return $this;
+        return $this->name;
     }
 
-    public function getCellar(): ?bool
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): string
     {
-        return $this->cellar;
-    }
-
-    public function setCellar(?bool $cellar): self
-    {
-        $this->cellar = $cellar;
-
-        return $this;
-    }
-
-    public function getGarden(): ?bool
-    {
-        return $this->garden;
-    }
-
-    public function setGarden(?bool $garden): self
-    {
-        $this->garden = $garden;
-
-        return $this;
-    }
-
-    public function getParking(): ?bool
-    {
-        return $this->parking;
-    }
-
-    public function setParking(?bool $parking): self
-    {
-        $this->parking = $parking;
-
-        return $this;
-    }
-
-    public function getBalcony(): ?bool
-    {
-        return $this->balcony;
-    }
-
-    public function setBalcony(?bool $balcony): self
-    {
-        $this->balcony = $balcony;
-
-        return $this;
-    }
-
-    public function getOpticalFiber(): ?bool
-    {
-        return $this->opticalFiber;
-    }
-
-    public function setOpticalFiber(?bool $opticalFiber): self
-    {
-        $this->opticalFiber = $opticalFiber;
-
-        return $this;
-    }
-
-    public function getIntercom(): ?bool
-    {
-        return $this->intercom;
-    }
-
-    public function setIntercom(?bool $intercom): self
-    {
-        $this->intercom = $intercom;
-
-        return $this;
-    }
-
-    public function getTerrace(): ?bool
-    {
-        return $this->terrace;
-    }
-
-    public function setTerrace(?bool $terrace): self
-    {
-        $this->terrace = $terrace;
-
-        return $this;
-    }
-
-    public function getSwimmingPool(): ?bool
-    {
-        return $this->swimmingPool;
-    }
-
-    public function setSwimmingPool(?bool $swimmingPool): self
-    {
-        $this->swimmingPool = $swimmingPool;
-
-        return $this;
+        $this->name = $name;
     }
 
     public function getEquipment(): ?Property
