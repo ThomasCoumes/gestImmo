d2df9d9cbccc222879b3e107f6671d43fa40507d
Thomas Coumes <thomas.coumes@smile.fr>
2019-02-18 09:51:41 +0100

    asserts

--- src/Entity/Equipment.php
+++ src/Entity/Equipment.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\EquipmentRepository")
@@ -15,11 +16,13 @@ class Equipment
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Assert\Type("integer")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Type("string")
      */
     private $name;
 
