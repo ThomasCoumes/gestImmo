33a2c95dd748b4869f5a65b68acdd4b389536553
Thomas Coumes <thomas.coumes@smile.fr>
2019-02-18 10:45:08 +0100

    asserts on Lessee property

--- src/Entity/Lessee.php
+++ src/Entity/Lessee.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\LesseeRepository")
@@ -13,41 +14,83 @@ class Lessee
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Assert\Type("integer")
+     * @Assert\NotBlank
+     * @var int
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Type("string")
+     * @Assert\Choice(choices={"Mr", "Mme", "Mlle"},
+     *      message="Veuillez choisir votre civilité")
+     * @Assert\NotBlank
+     * @var string
      */
     private $civility;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Type("string")
+     * @Assert\NotBlank
+     * @var string
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Type("string")
+     * @var string
      */
     private $lastname;
 
     /**
      * @ORM\Column(type="date")
+     * @Assert\NotBlank
+     * @Assert\Date
+     * @var string A "Y-m-d" formatted value
      */
     private $birthday;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Type("string")
+     * @var string
      */
     private $placeOfBirth;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Type("string")
+     * @Assert\Email(
+     *     message = "Veuillez renseigner votre adresse mail.",
+     *     checkMX = true
+     * )
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 255,
+     *      minMessage = "Votre email est trop court",
+     *      maxMessage = "Votre email est trop long"
+     * )
+     * @var string
      */
     private $email;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Type("string")
+     * @Assert\Length(
+     *      min = 10,
+     *      max = 17,
+     *      minMessage = "Votre email est trop court",
+     *      maxMessage = "Votre email est trop long"
+     * )
+     * @var string
      */
     private $phoneNumber;
 
