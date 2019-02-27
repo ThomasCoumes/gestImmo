f56a4022c1d4a153835eb2e22c0a4adacf34ab0d
Thomas Coumes <thomas.coumes@smile.fr>
2019-02-27 11:08:21 +0100

    LESSEE is redirected to property index at login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 /**
- * @IsGranted("ROLE_USER")
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
  */
 class HomeController extends AbstractController
 {
     /**
      * @Route("/accueil", name="home")
      * @return \Symfony\Component\HttpFoundation\Response
+     * @internal param User $user
      */
     public function index()
     {
+        $user = $this->getUser();
+
+        if (in_array('ROLE_LESSEE', $user->getRoles())) {
+            return $this->redirectToRoute('property_index');
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
