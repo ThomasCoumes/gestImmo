8d6fb68ee263907d3b7f1f18d4a78adeed2ff4ca
Thomas Coumes <thomas.coumes@smile.fr>
2019-02-27 10:50:14 +0100

    LESSEES can't access to ROLE_USER methods

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -200,7 +200,7 @@ class User implements UserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[] = '';
 
         return array_unique($roles);
     }
