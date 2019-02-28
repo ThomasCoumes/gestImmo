a06a011d21fb0773208016b85f6355f0edb2e6fe
Thomas Coumes <thomas.coumes@smile.fr>
2019-02-28 08:46:42 +0100

    composer require symfony/acl-bundle

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+
 /**
  * @Route("/proprietees")
  */
