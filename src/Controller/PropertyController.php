6449fd070f941031170f4e468b790d9c05fd3845
Thomas Coumes <thomas.coumes@smile.fr>
2019-02-28 08:56:55 +0100

    PSR fix

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -13,7 +13,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-
 /**
  * @Route("/proprietees")
  */
