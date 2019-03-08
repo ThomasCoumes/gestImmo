0e1de8e862d128eee41f157385d74c1db8c6ed99
Thomas Coumes <thomas.coumes@smile.fr>
2019-03-08 16:00:05 +0100

    basic style on rentRelease

--- src/Controller/RentReleaseController.php
+++ src/Controller/RentReleaseController.php
@@ -5,13 +5,12 @@ namespace App\Controller;
 use App\Entity\RentRelease;
 use App\Repository\RentReleaseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
- * @Route("/rent-release")
+ * @Route("/loyers")
  * @IsGranted("ROLE_USER")
  */
 class RentReleaseController extends AbstractController
