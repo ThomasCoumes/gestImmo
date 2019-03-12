380f8ed3e2e52ced4bee4ed47b5dda94ab0505be
Thomas Coumes <thomas.coumes@smile.fr>
2019-03-12 14:02:29 +0100

    sending email to owners

--- src/Service/MonthlyMailer.php
+++ src/Service/MonthlyMailer.php
@@ -9,6 +9,8 @@
 namespace App\Service;
 
 use App\Repository\LesseeRepository;
+use Swift_Mailer;
+use Twig_Environment;
 
 class MonthlyMailer
 {
@@ -16,14 +18,26 @@ class MonthlyMailer
      * @var LesseeRepository
      */
     private $lesseeRepository;
+    /**
+     * @var Swift_Mailer
+     */
+    private $mailer;
+    /**
+     * @var Twig_Environment
+     */
+    private $twig;
 
     /**
      * MonthlyMailer constructor.
      * @param LesseeRepository $lesseeRepository
+     * @param Swift_Mailer $mailer
+     * @param Twig_Environment $twig
      */
-    public function __construct(LesseeRepository $lesseeRepository)
+    public function __construct(LesseeRepository $lesseeRepository, Swift_Mailer $mailer, Twig_Environment $twig)
     {
         $this->lesseeRepository = $lesseeRepository;
+        $this->mailer = $mailer;
+        $this->twig = $twig;
     }
 
     public function notifyOwner()
@@ -32,12 +46,26 @@ class MonthlyMailer
         $mailList = [];
         foreach ($lesseeRepository as $lessee) {
             $mails = $lessee->getUserLessee()->getEmail();
+            $name = $lessee->getUserLessee()->getName();
             if (!in_array($mails, $mailList)) {
                 $mailList[] = $mails;
             }
         }
 
-        dump($mailList);
+        foreach ($mailList as $mail) {
+            $message = (new \Swift_Message('Nous avons generer vos loyers !'))
+                // set the email you defined in .env.local here
+                ->setFrom('thomascoumes3145@gmail.com')
+                ->setTo("$mail")
+                ->setBody(
+                    $this->twig->render(
+                        'rent_release/emailOwner.html.twig'
+                    ),
+                    'text/html'
+                );
+            $this->mailer->send($message);
+        }
 
+        return true;
     }
 }
