d2268c7f57e6705b69cbf81ac5081ea0e2672b0a
Thomas Coumes <thomas.coumes@smile.fr>
2019-03-18 14:59:33 +0100

    deleting useless code

--- src/Controller/ResumePageController.php
+++ src/Controller/ResumePageController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Repository\RentReleaseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Form\Exception\LogicException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
